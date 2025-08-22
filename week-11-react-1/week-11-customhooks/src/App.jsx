bb0b3d72cffe156bb5db2ef84dd043fadb0821e8
parvatisanthosh <parvathygsanthosh@gmail.com>
2025-08-22 11:28:07 +0530

    added a basic custom hook

--- week-11-react-1/week-11-customhooks/src/App.jsx
+++ week-11-react-1/week-11-customhooks/src/App.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react'
 
 import './App.css'
+ function useCounter(){
+  const [count ,setCount ] = useState(1);
+  function Setcounter(){
+    setCount(count + 1);
+  }
 
+  return ({
+    count : count,
+    Setcounter : Setcounter
+  })
+ }
 function App() {
 
 
   return (
     <div>
-      hello there
+     <Counter></Counter>
+     <Counter></Counter>
+     <Counter></Counter>
+     <Counter></Counter>
     </div>
   )
+}
+
+function Counter(){
+  const  { count ,Setcounter } = useCounter();
+  return <div><button onClick={Setcounter}>count : {count} </button>
+</div>
 }
 
 export default App
